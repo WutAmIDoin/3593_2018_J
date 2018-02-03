c7ba0bdc9edaf35a4c7893f8227f9ee60c705bda
Mason <emperor@emperorof.space>
2018-02-03 17:07:15 -0600

    Created DriveSubsystem and added a couple methods

--- src/org/usfirst/frc/team3593/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team3593/robot/subsystems/DriveSubsystem.java
@@ -5,6 +5,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
@@ -16,6 +17,8 @@ public class DriveSubsystem extends Subsystem {
 	Spark driveRight;
 	Solenoid shifter;
 	
+	DifferentialDrive drive;
+	
 	AnalogGyro gyro;
 	Encoder leftEncoder;
 	Encoder rightEncoder;
@@ -26,10 +29,27 @@ public class DriveSubsystem extends Subsystem {
 		driveRight = new Spark(RobotMap.driveRightPWM);
 		shifter = new Solenoid(RobotMap.shifterSolendoidChannel);
 		
+		drive = new DifferentialDrive(driveLeft, driveRight);
+		
 		gyro = new AnalogGyro(RobotMap.gyroChannel);
 		leftEncoder = new Encoder(RobotMap.leftEncoder1, RobotMap.leftEncoder2);
 		rightEncoder = new Encoder(RobotMap.rightEncoder1, RobotMap.rightEncoder2);
 		ntVision = NetworkTableInstance.getDefault().getTable("vision");
+	}
+	
+	public void driveTank(double left, double right) {
+		drive.tankDrive(left, right);
+	}
+	
+	Boolean shifterVar = false;
+	long timeout = System.nanoTime();
+	public void toggleShifter() {
+		if(System.nanoTime() > timeout)
+		{
+			shifter.set(shifterVar);
+			shifterVar = !shifterVar;
+			timeout = System.nanoTime() + 2000000000;
+		}
 	}
 	
     // Put methods for controlling this subsystem
