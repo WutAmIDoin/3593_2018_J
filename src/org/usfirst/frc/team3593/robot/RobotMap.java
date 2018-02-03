4a9a1ad344aaa1cec792da4fa2e31bcdb2a9a7b4
Carol Xu <jiaman1122@gmail.com>
2018-02-03 17:07:39 -0600

    DriveSubsystem

--- src/org/usfirst/frc/team3593/robot/RobotMap.java
+++ src/org/usfirst/frc/team3593/robot/RobotMap.java
@@ -7,20 +7,40 @@
 
 package org.usfirst.frc.team3593.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
-public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
 
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+public class RobotMap {
+	
+	//PWMs
+	public static int driveLeftPWM = 1;
+	public static int driveRightPWM = 0;
+	public static int shooterFrontLeft = 5;
+	public static int shooterRearLeft = 6;
+	public static int shooterFrontRight = 7;
+	public static int shooterRearRight = 9;
+	
+	
+	
+	//5678
+	//Solenoids:
+	public static int shifter = 4;
+	public static int booster = 5;
+	public static int lefter = 6;
+	public static int flap = 7; 
+	
+	
+	//analog
+	public static int gyro = 3;
+	public static int pressureSensor = 4;
+	
+	
+	//DIO
+	public static int encoderLeft1 = 0;
+	public static int encoderLeft2 = 1;
+	public static int encoderRight1 = 2;
+	public static int encoderRight2 = 3;
+	
+	//network Table
+	
+	
+	
 }
---
+++ src/org/usfirst/frc/team3593/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team3593.robot.subsystems;
+
+import org.usfirst.frc.team3593.robot.RobotMap;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+
+
+
+public class DriveSubsystem extends Subsystem {
+
+	Spark driveLeft;
+	Spark driveRight;
+	Solenoid shifter;
+	AnalogGyro gyro;
+	Encoder encoderLeft;
+	Encoder encoderRight;
+	NetworkTable ntVision;
+	DifferentialDrive drive;
+	
+	
+	public DriveSubsystem() {
+		driveLeft = new Spark(RobotMap.driveLeftPWM);
+		driveRight = new Spark(RobotMap.driveRightPWM);
+		shifter = new Solenoid(RobotMap.shifter);
+		gyro = new AnalogGyro(RobotMap.gyro);
+		encoderLeft = new Encoder(RobotMap.encoderLeft1, RobotMap.encoderLeft2);
+		encoderRight = new Encoder(RobotMap.encoderRight1, RobotMap.encoderRight2);
+		ntVision = NetworkTableInstance.getDefault().getTable("Vision");
+		//get default table then track "vision" table
+		drive = new DifferentialDrive(driveLeft, driveRight);
+	}
+	
+	public void driveTank(double left, double right) {
+		drive.tankDrive(left, right);
+	}
+	
+	
+	Boolean shifterVar = false;
+	long timeout = System.nanoTime();
+	public void toggleShifter() {
+		if(System.nanoTime()>timeout) {
+			shifter.set(shifterVar);
+			shifterVar =! shifterVar;
+			timeout = System.nanoTime() + 2000000000;
+		}
+	}
+    
+			
+    public void initDefaultCommand() {	
+    }
+}
+
